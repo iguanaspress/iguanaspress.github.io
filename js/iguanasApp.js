8a1ccfd6182b8405de283eb629fe4a5d0b12d1d2
Laura Rodriguez <lauraisabelroser@gmail.com>
2017-03-16 15:51:05 +0100

    social

--- js/iguanasApp.js
+++ js/iguanasApp.js
@@ -1,8 +1,23 @@
-$(document).ready(function()
-{
+var templates_items, templates_text;
+
+function showTemplate(template, data){
+	var html    = template(data);
+	$('.content').html(html);
+}
+
+
+$(document).ready(function(){
+        var fuente   = $("#formulaire").html();
+        templates_forms = Handlebars.compile(fuente);    
+    
+    
+    
     $('.carousel').carousel({
-      interval: 6000
+      interval: 5000
     });
 
     $('.carousel').carousel('next');
+    
+    
+    
 });
